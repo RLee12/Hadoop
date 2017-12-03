9bdf7d104be784e2dd3e603da79ffb8e4c17113a
RayLee <m519160201@gmail.com>
2017-12-03 14:55:56 -0800

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,8 +1,10 @@
-
 import sys
 import re
 
 reload(sys)
+
+# Change the encoding to utf-8, since there are many non-English 
+# characters in the dataset.
 sys.setdefaultencoding("utf-8")
 
 stop_words = [unicode(line.strip()) for line in open("stop_words_en.txt").readlines()]
@@ -12,7 +14,12 @@ for line in sys.stdin:
         article_id, text = unicode(line.strip()).split('\t', 1)
     except ValueError as e:
         continue
+    
+    # Remove special characters at the start or at the end of any string.
     text = re.sub("^\W+|\W+$", "", text, flags=re.UNICODE)
+    
+    # Split words by any non-word characters and white space.
     words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
+    
     for word in words:
-        print "%s\t%d" % (word, 1)
+        print "%s\t%d" % (word, 1) # Output (key, value) pair.
