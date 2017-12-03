72c89d3e67c613de6d2421af9ea7399192b50299
RayLee <m519160201@gmail.com>
2017-12-03 14:59:24 -0800

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,4 +1,3 @@
-
 import sys
 
 current_key = None
@@ -10,11 +9,15 @@ for line in sys.stdin:
         count = int(count)
     except ValueError as e:
         continue
+    
+    # Notice that keys are shuffled before reduce phase, so in the streaming
+    # we are expecting same keys in a continuous order.
     if current_key != key:
         if current_key:
             print "%s\t%d" % (current_key, word_sum)
         word_sum = 0
         current_key = key
+    
     word_sum += count
 
 if current_key:
