db40d640d39213ac975a4bb968703989c63b5f16
RayLee <m519160201@gmail.com>
2017-12-05 18:33:22 -0800

    Update mapper_word_group_job2.py

--- mapper_word_group_job2.py
+++ mapper_word_group_job2.py
@@ -3,4 +3,7 @@ import sys
 for line in sys.stdin:
     word, length, count = line.strip().split("\t")
     if word:
+        
+        # Sort each word before outputing them, since words with different permutations will be exactly the 
+        # same after being sorted.
         print "%s\t%s\t%d" % ("".join(sorted(word)), word, int(count))
