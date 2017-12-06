a2289301dd23775f44816bd6155dd9828680961a
RayLee <m519160201@gmail.com>
2017-12-05 18:25:35 -0800

    Update mapper_word_group_job1.py

--- mapper_word_group_job1.py
+++ mapper_word_group_job1.py
@@ -2,6 +2,9 @@ import sys
 import re
 
 reload(sys)
+
+# Change the encoding to utf-8, since there are many non-English 
+# characters in the dataset.
 sys.setdefaultencoding("utf-8")
 
 stop_words = [unicode(line.strip()) for line in open("stop_words_en.txt").readlines()]
@@ -11,9 +14,14 @@ for line in sys.stdin:
         article_id, text = unicode(line.strip()).split('\t', 1)
     except ValueError as e:
         continue
+    
+    # Remove special characters at the start or at the end of any string.
     text = re.sub("^\W+|\W+$", "", text, flags=re.UNICODE)
+    
+    # Split words by any non-word characters and white space.
     words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
+    
     for word in words:
         if word:
             if word.lower() not in stop_words:
-                print "%s\t%d" % (word.lower(), 1)
+                print "%s\t%d" % (word.lower(), 1) # Output (key, value) pair.
